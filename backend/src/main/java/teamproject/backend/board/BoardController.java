c407cfc24c6292e6a081a25c631608a8a5477d17
ks12b0000 <ks12b0000@gmail.com>
2022-11-27 22:49:27 +0900

    CI CD Test

--- backend/src/main/java/teamproject/backend/board/BoardController.java
+++ backend/src/main/java/teamproject/backend/board/BoardController.java
@@ -1,35 +1,35 @@
-package teamproject.backend.board;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import teamproject.backend.board.dto.BoardReadResponse;
-import teamproject.backend.board.dto.BoardWriteRequest;
-import teamproject.backend.response.BaseResponse;
-import teamproject.backend.response.ValidationSequence;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-public class BoardController {
-    private final BoardService boardService;
-
-    @PostMapping("/auth/board/write")
-    public BaseResponse board_write(@Validated(ValidationSequence.class) @RequestBody BoardWriteRequest boardWriteRequest){
-        boardService.save(boardWriteRequest);
-        return new BaseResponse("성공적으로 글이 작성됐습니다.");
-    }
-
-    @GetMapping("/board/list")
-    public BaseResponse<List<BoardReadResponse>> board_list(@RequestParam String category, @RequestParam int page){
-        List<BoardReadResponse> pages = boardService.getBoards(category, page);
-        return new BaseResponse<>("성공적으로 글을 가져왔습니다.", pages);
-    }
-
-    @GetMapping("/board")
-    public BaseResponse<BoardReadResponse> search_board(@RequestParam Long board_id){
-        BoardReadResponse boardReadResponse = boardService.findById(board_id);
-        return new BaseResponse<>("성공적으로 글을 가져왔습니다.", boardReadResponse);
-    }
-}
+//package teamproject.backend.board;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//import teamproject.backend.board.dto.BoardReadResponse;
+//import teamproject.backend.board.dto.BoardWriteRequest;
+//import teamproject.backend.response.BaseResponse;
+//import teamproject.backend.response.ValidationSequence;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class BoardController {
+//    private final BoardService boardService;
+//
+//    @PostMapping("/auth/board/write")
+//    public BaseResponse board_write(@Validated(ValidationSequence.class) @RequestBody BoardWriteRequest boardWriteRequest){
+//        boardService.save(boardWriteRequest);
+//        return new BaseResponse("성공적으로 글이 작성됐습니다.");
+//    }
+//
+//    @GetMapping("/board/list")
+//    public BaseResponse<List<BoardReadResponse>> board_list(@RequestParam String category, @RequestParam int page){
+//        List<BoardReadResponse> pages = boardService.getBoards(category, page);
+//        return new BaseResponse<>("성공적으로 글을 가져왔습니다.", pages);
+//    }
+//
+//    @GetMapping("/board")
+//    public BaseResponse<BoardReadResponse> search_board(@RequestParam Long board_id){
+//        BoardReadResponse boardReadResponse = boardService.findById(board_id);
+//        return new BaseResponse<>("성공적으로 글을 가져왔습니다.", boardReadResponse);
+//    }
+//}
--- backend/src/main/java/teamproject/backend/board/BoardRepository.java
+++ backend/src/main/java/teamproject/backend/board/BoardRepository.java
@@ -1,11 +1,13 @@
-package teamproject.backend.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamproject.backend.domain.Board;
-import teamproject.backend.domain.FoodCategory;
-
-import java.util.List;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findByCategory(FoodCategory foodCategory);
-}
+//package teamproject.backend.board;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import teamproject.backend.domain.Board;
+//import teamproject.backend.domain.FoodCategory;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface BoardRepository extends JpaRepository<Board, Long> {
+//    List<Board> findByCategory(FoodCategory foodCategory);
+//}
