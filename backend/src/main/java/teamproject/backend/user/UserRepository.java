ad6eeda5b58b6c88056a362b1f90f9e1a824e4f4
ks12b0000 <ks12b0000@gmail.com>
2022-11-27 23:46:53 +0900

    CI CD Test

--- backend/src/main/java/teamproject/backend/user/UserRepository.java
+++ backend/src/main/java/teamproject/backend/user/UserRepository.java
@@ -1,8 +1,10 @@
 package teamproject.backend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import teamproject.backend.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findByUsernameAndPassword(String username, String password);
