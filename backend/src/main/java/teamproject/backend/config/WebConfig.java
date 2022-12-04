63eb0498068409b0adc35f94a2879894d9f387d8
ks12b0000 <ks12b0000@gmail.com>
2022-12-04 17:06:35 +0900

    Token 확인 테스트

--- backend/src/main/java/teamproject/backend/config/WebConfig.java
+++ backend/src/main/java/teamproject/backend/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .exposedHeaders("accessCookie");
+                .exposedHeaders("Set-Cookie");
     }
 }
