8a4814526aff19659ac10f3053a2e9c381dfa50a
mim-mel <mfblade2014@gmail.com>
2022-12-07 16:59:18 +0900

    feat: 유저 정보를 전역 상태로 관리하기 위한 UserSlice 생성

---
+++ front/src/redux/reducer/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const UserSlice = createSlice({
+    name: 'user',
+    initialState: {
+        userId : '',
+        isLoggedIn : false,
+    },
+    reducer: {
+        loginUser: (state, action) => {
+            state.userId = action.payload.userId;
+            state.isLoggedIn = action.payload.isLoggedIn;
+        },
+
+        logoutUser: (state) => {
+            state.userId = '';
+            state.isLoggedIn = false;
+        }
+    }
+})
+
+export const {loginUser, logoutUser} = UserSlice.actions;
+
+export default UserSlice.reducer;
