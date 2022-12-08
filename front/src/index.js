010064bd8e87030b01949af9ee174d6c029b5639
mim-mel <mfblade2014@gmail.com>
2022-12-08 17:49:18 +0900

    feat: redux-persist 적용

--- front/src/index.js
+++ front/src/index.js
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import store, {persistor} from "./redux/store/store";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
             <Provider store={store}>
-                <App />
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
             </Provider>
     </BrowserRouter>
 );
