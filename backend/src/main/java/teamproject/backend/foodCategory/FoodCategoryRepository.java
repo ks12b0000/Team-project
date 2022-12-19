c895c0ef131ad3c7e020e886707b505ecbb5cbed
mim-mel <mfblade2014@gmail.com>
2022-12-19 09:17:01 +0900

    remove: 백엔드 폴더 삭제

--- backend/src/main/java/teamproject/backend/foodCategory/FoodCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package teamproject.backend.foodCategory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamproject.backend.domain.FoodCategory;
-
-public interface FoodCategoryRepository extends JpaRepository<FoodCategory, Long> {
-    FoodCategory findByCategoryName(String categoryName);
-}
--- backend/src/main/java/teamproject/backend/response/ValidationGroup.java
+++
@@ -1,9 +0,0 @@
-package teamproject.backend.response;
-
-public class ValidationGroup {
-
-    public interface NotBlankGroup{}
-    public interface NotNullGroup{}
-    public interface PatternGroup{}
-    public interface EmailGroup{}
-}
